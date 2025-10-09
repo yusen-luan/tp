298fcfeb48c575a8bee827beee8cfd0c367793f0
yusen-luan <luanyusen@gmail.com>
2025-10-09 09:46:00 +0800

    Add alternate constructor in Person

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -30,6 +30,21 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags,
+        StudentId studentId, ModuleCode moduleCode) {
+        requireAllNonNull(name, phone, email, address, tags);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.tags.addAll(tags);
+        this.studentId = studentId;
+        this.moduleCode = moduleCode;
+    }
+
+    /**
+     * Alternate constructor for non students
+     */
     public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
@@ -37,6 +52,8 @@ public class Person {
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.studentId = null;
+        this.moduleCode = null;
     }
 
     public Name getName() {
